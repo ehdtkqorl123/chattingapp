e4d0628e84381546597bca070bca554e82a4588c
Paul Shin <thegreatman@gmail.com>
2017-03-25 20:16:48 -0400

    - Initial commit

---
+++ app/src/main/java/chatting/paulshin/ca/chattingapp/MainActivity.java
@@ -0,0 +1,13 @@
+package chatting.paulshin.ca.chattingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
